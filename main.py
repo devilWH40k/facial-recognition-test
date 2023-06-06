40c796d5e9c2ea8429d3e76d827333268a51c7fb
XUSdLfXD <devilwork123123@gmail.com>
2023-06-06 17:35:31 +0300

    some cleanups

--- main.py
+++ main.py
@@ -2,10 +2,6 @@ import pandas as pd
 from deepface import DeepFace
 from core.face_model import FaceModel
 
-# temporary
-import os
-import numpy as np
-
 from utils import make_dataset
 
 def main():
@@ -15,28 +11,16 @@ def main():
 
     # Define models
     models = [
-        # FaceModel("VGG-Face"), 
-        # FaceModel("Facenet"), 
-        # FaceModel("Facenet512"), 
-        # FaceModel("OpenFace"), 
-        # FaceModel("DeepFace"), 
+        FaceModel("VGG-Face"), 
+        FaceModel("Facenet"), 
+        FaceModel("Facenet512"), 
+        FaceModel("OpenFace"), 
+        FaceModel("DeepFace"), 
         FaceModel("ArcFace"), 
         FaceModel("SFace"),
     ]
 
     results = {}
-
-    # corupted_files = []
-
-    # for files in dataset[1000:]:
-    #     for file in files:
-    #         try:
-    #             embedding_objs = DeepFace.represent(img_path = file)
-    #         except Exception:
-    #             corupted_file = file
-    #             if corupted_file not in corupted_files:
-    #                 corupted_files.append(corupted_file)
-    #                 print(corupted_file, "[corupted]")
 
     for model in models:
         result = model.evaluate(DeepFace.verify, dataset)
@@ -44,10 +28,6 @@ def main():
 
     table = pd.DataFrame(results)
     print(table)
-
-    # Store resutls in a csv file
-    # pd.DataFrame(results).transpose().to_csv("scores.csv")
-
     print("Done!")
 
 
